c712480166dfbf49398b4bc7c3570dadfe28cc5b
David Krawchuk <krawchukdavid@gmail.com>
2015-04-12 20:01:08 -0500

    Added base Checklist View Controllers

---
+++ ChecklistsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChecklistsTableViewController.h
+//  Emergency_Wisconsin_IOS
+//
+//  Created by David Krawchuk on 4/12/15.
+//  Copyright (c) 2015 UniversityOfWisconsinParkside. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChecklistsTableViewController : UITableViewController
+
+@end
---
+++ ItemAddViewController.m
@@ -0,0 +1,37 @@
+//
+//  ItemAddViewController.m
+//  Emergency_Wisconsin_IOS
+//
+//  Created by David Krawchuk on 4/12/15.
+//  Copyright (c) 2015 UniversityOfWisconsinParkside. All rights reserved.
+//
+
+#import "ItemAddViewController.h"
+
+@interface ItemAddViewController ()
+
+@end
+
+@implementation ItemAddViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
